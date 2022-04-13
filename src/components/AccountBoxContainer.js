08693242d5adae04b2873bef2ca9c022b5fd32c4
Philip Schotte <philipschotte@gmail.com>
2022-04-13 18:38:12 -0500

    Updated Account page

--- src/components/AccountBoxContainer.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const NameOuterContainer = styled.View`
-height: 92px;
-width: 358px;
-margin-left: 16
-border-radius: 0px;
-
-`
---
+++ src/components/NameContainer.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const AccountBoxContainer = styled.View`
+height: 92px;
+width: 358px;
+margin-left: 16px;
+margin-right: 16px;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+`
