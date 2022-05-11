cb99343961f5dd68bf7734099181461e0d24d327
hanstoj <stojack.hannah@gmail.com>
2022-05-11 18:33:53 -0500

    current expense flow

--- src/components/CreateGroupForm.js
+++ src/components/CreateGroupForm.js
@@ -32,6 +32,7 @@ display: flex;
 
 const FormContainer  = styled.View`
 margin: 16px;
+display: flex ;
 
 `
 const TitleText  = styled.Text`
@@ -47,22 +48,57 @@ font-size: 21px;
 font-weight: 700;
 text-align: left;
 `
+
+const SmallText  = styled.Text`
+font-size: 15px;
+text-align: left;
+margin: 5px ;
+`
+
 const InputText  = styled.TextInput`
 height: 40px;
 width: 360px;
 margin: 12px;
 padding: 20px;
-border: 2px grey solid;
+border: 2px #9E9E9E solid;
+border-radius: 6px;
+`
+const InputTextArea  = styled.TextInput`
+
+margin: 12px;
+padding: 20px;
+border: 2px #9E9E9E solid;
 border-radius: 6px;
 `
 
+
 const DateEntry  = styled.TextInput`
 height: 40px;
-width: 360px;
+width: 130px;
 margin: 12px;
 padding: 20px;
-border: 2px grey solid;
+border: 2px #9E9E9E solid;
 border-radius: 6px;
+`
+
+const Continue = styled.View`
+height: 48px;
+width: 110%;
+background-color: #616161;
+align-items: center;
+align-self: flex-end;
+margin-top: auto;
+`
+
+const ContinueText = styled.Text`
+height: 26px;
+width: 104px;
+margin-top: 11px;
+margin-bottom: 11px;
+font-family: 'System';
+font-size: 17px;
+text-align: center;
+color: #F5F5F5;
 `
 
 
@@ -87,7 +123,11 @@ export function CreateGroupForm() {
         placeholder="Tap to add"
       />
          <LabelText>Who is Involved?</LabelText>
-
+         <SmallText>Group Total: 0</SmallText>
+         <InputTextArea 
+    numberOfLines={4}
+    placeholder="Start adding names here" >
+         </InputTextArea>
         </FormContainer>
     )
 }
